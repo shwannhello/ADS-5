e144b3efdfa5fd1091a103c3a288d5b75d61d999
shwannhello <shvarovaann@yandex.ru>
2026-04-18 01:32:16 +0300

    Шварова 25 ист-3

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,35 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[size];
+    int top_idx;
+public:
+    TStack() : top_idx(-1) {}   
+    void push(T value) {
+        if (top_idx < size - 1) {
+            arr[++top_idx] = value;
+        }
+    }  
+    T pop() {
+        if (top_idx >= 0) {
+            return arr[top_idx--];
+        }
+        return T();
+    }
+    T top() const {
+        if (top_idx >= 0) {
+            return arr[top_idx];
+        }
+        return T();
+    }
+    bool empty() const {
+        return top_idx == -1;
+    }
+    int size() const {
+        return top_idx + 1;
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
